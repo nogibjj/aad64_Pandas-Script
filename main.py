d547bdd417aa6aef37c31c7ce9a7e962919e203e
AaryaDesai1 <143753050+AaryaDesai1@users.noreply.github.com>
2023-09-09 21:42:22 +0000

    added functions to main.py and test_main.py

--- main.py
+++ main.py
@@ -1,10 +1,54 @@
 """Importing module pandas for my function."""
 import pandas as pd
+import matplotlib.pyplot as plt
 df = pd.read_csv("iris.csv")
 
-def avg_88(data):
+def average(data):
     """"This is a mean function."""
     result = data.mean()
     return result
 
-print(avg_88(df.iloc[:, 1]))
+def med(data):
+    """"This is a median function."""
+    result = data.median()
+    return result
+
+def standard_deviation(data):
+    """"This is a standard deviation function."""
+    result = data.std()
+    return result
+
+#print(average(df.iloc[:, 1]))
+#print(med(df.iloc[:, 1]))
+#print(standard_deviation(df.iloc[:, 1]))
+
+def visualize_data(data, x_column, y_column, plot_type='line', title=None, xlabel=None, ylabel=None):
+    """
+    Visualize data from a DataFrame using pandas' built-in plotting capabilities.
+
+    Args:
+        data (pd.DataFrame): The DataFrame containing the data.
+        x_column (str): The column to be used for the x-axis.
+        y_column (str): The column to be used for the y-axis.
+        plot_type (str, optional): Type of plot (e.g., 'line', 'scatter', 'bar'). Defaults to 'line'.
+        title (str, optional): Title for the plot. Defaults to None.
+        xlabel (str, optional): Label for the x-axis. Defaults to None.
+        ylabel (str, optional): Label for the y-axis. Defaults to None.
+
+    Returns:
+        None
+    """
+    plt.figure(figsize=(10, 6))
+    plt.scatter(data[x_column], data[y_column])
+    plt.title(title)
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    plt.show()
+
+# Example Usage of Visualization function:
+#data = {'x': [1, 2, 3, 4, 5], 'y': [2, 3, 5, 7, 11]}
+#df1 = pd.DataFrame(data)
+
+#visualize_data(df1, 'x', 'y', plot_type='line', title='Line Plot Example', xlabel='X-axis', ylabel='Y-axis')
+
+
--- test_main.py
+++ test_main.py
@@ -1,10 +1,13 @@
-from main import avg_88
+from main import average, med, standard_deviation, visualize_data
 import pandas as pd
 
 dataset = {"Values": [1, 3, 5, 7, 9]}
 testing_data = pd.DataFrame(dataset)
-def testing_main():
-    assert avg_88(testing_data['Values']) == 5
-    print("the mean is 5")
-    
-testing_main()
+def testing_main_avg():
+    assert average(testing_data['Values']) == 5
+
+def testing_main_med():
+    assert med(testing_data['Values']) == 5
+
+def testing_main_std():
+    assert standard_deviation(testing_data['Values']) == 3.1622776601683795
